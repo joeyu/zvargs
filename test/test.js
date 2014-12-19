67378a8199e35c56f2bcdde170a72c39e0b5806b
Zhou Yu <nap.zhou.yu@gmail.com>
2014-12-19 21:58:09 +0800

    Added Args.__arguments and parse string representation

--- test/test.js
+++ test/test.js
@@ -12,14 +12,33 @@ describe('VArgs', function(){
         //console.log(args);
         (function () {
             var args = new zvargs.VArgs(arguments, spec);
-            var argsParent = arguments;
-            //console.log(arguments);
+            var self = arguments;
+            //console.log(args.__arguments);
 
             it(util.inspect(arguments), function(){
                 assert.equal(args.int1, 1);
                 assert.equal(args.int2, null);
                 assert.equal(args.regexp1.toString(), /regexp/.toString());
-                assert.equal(args.__extra.toString(), Array.prototype.slice.call(argsParent, 2).toString());
+                assert.equal(args.__extra.toString(), Array.prototype.slice.call(self, 2).toString());
+                //assert.equal(args.__arguments.toString(), Array.prototype.slice.call(self, 0).toString());
+            })
+        })(1, /regexp/, 2, 3, 'b', 'c', 'd', 'e', 'f');
+    });
+    describe('#constructor()', function(){
+        var spec = "int1:number, [int2:number], [regexp1: RegExp]";
+
+        //console.log(args);
+        (function () {
+            var args = new zvargs.VArgs(arguments, spec);
+            var self = arguments;
+            //console.log(args.__arguments);
+
+            it(util.inspect(arguments), function(){
+                assert.equal(args.int1, 1);
+                assert.equal(args.int2, null);
+                assert.equal(args.regexp1.toString(), /regexp/.toString());
+                assert.equal(args.__extra.toString(), Array.prototype.slice.call(self, 2).toString());
+                //assert.equal(args.__arguments.toString(), Array.prototype.slice.call(self, 0).toString());
             })
         })(1, /regexp/, 2, 3, 'b', 'c', 'd', 'e', 'f');
     })
--- zvargs.js
+++ zvargs.js
@@ -4,8 +4,37 @@
 module.exports.VArgs = VArgs;
 
 function VArgs(args, spec) {
+    this.__arguments = [];
     var i, j;
+
+    if (typeof spec == 'string') {
+        spec = spec.split(',').map(function(s) {
+            s = s.trim();
+            m = s.match(/^(\[)?\s*(\w*)\s*\:\s*(\w*)\s*(\])?$/);
+            if (m) {
+                if (m[1] === '[' && m[4] === ']') {
+                    s = {'name': m[2], 'type': m[3], 'optional': true};
+                } else {
+                    s = {'name': m[2], 'type': m[3]};
+                }
+
+                if (   s.type !== 'number' 
+                    && s.type !== 'string' 
+                    && s.type !== 'boolean' 
+                    && s.type !== 'function' 
+                    && s.type !== 'object' 
+                    && s.type !== 'symbol') {
+
+                    s.type = eval(s.type);
+                }
+
+                return s;
+            }
+        });
+    }
+
     for (j = 0; j < spec.length; j ++) {
+        this.__arguments[j] = null;
         this[spec[j].name] = null;
     }
     
@@ -34,6 +63,7 @@ function VArgs(args, spec) {
             }
 
             // matched
+            this.__arguments[j] = args[i];
             this[spec[j++].name] = args[i];
             break;
         }
@@ -43,7 +73,6 @@ function VArgs(args, spec) {
     for(;j < spec.length; j++) {
        if (!spec[j].hasOwnProperty('optional') || !spec[j].optional) {
            throw "Error: not all mandatory arguments are passed";
-           continue;
        }
     }
 
@@ -52,5 +81,8 @@ function VArgs(args, spec) {
     // Stores remaining args items in `extra`.
     if (i < args.length) {
         this.__extra = Array.prototype.slice.call(args, i);
+        this.__arguments = this.__arguments.concat(this.__extra);
     }
+
 }
+
